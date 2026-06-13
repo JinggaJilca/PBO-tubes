17291f3878ca1a906886b2ceaf0693d8a9ec26fc
naeyara <nayyaraaureliaputri@gmail.com>
2026-06-13 11:57:31 +0700

    update generate dummy budgets

--- database/pbo-fintrack.sql
+++ database/pbo-fintrack.sql
@@ -293,7 +293,6 @@ BEGIN
     DECLARE done        INT DEFAULT 0;
     DECLARE uid         INT;
     DECLARE cat_id      INT;
-    DECLARE total_bud   DECIMAL(18,2);
     DECLARE cat_bud     DECIMAL(18,2);
 
     DECLARE cur CURSOR FOR SELECT user_id FROM users;
@@ -311,14 +310,12 @@ BEGIN
         ORDER BY RAND()
         LIMIT 1;
 
-        SET total_bud = ROUND(RAND() * 4000000 + 1000000, 2);
-        SET cat_bud   = ROUND(total_bud * (0.2 + RAND() * 0.4), 2);
+        SET cat_bud = ROUND(500000 + (RAND() * 3000000), 2);
 
         INSERT INTO budgets (user_id, category_id, category_budget, threshold, start_date, end_date)
         VALUES (
             uid,
             cat_id,
-            total_bud,
             cat_bud,
             ROUND(50 + RAND() * 40, 2),                         -- threshold 50–90%
             DATE_FORMAT(NOW(), '%Y-%m-01'),                      -- awal bulan ini
@@ -330,7 +327,6 @@ BEGIN
 END$$
 DELIMITER ;
 CALL generate_budgets();
-fintrack_db
 
 -- generate transactions
 
