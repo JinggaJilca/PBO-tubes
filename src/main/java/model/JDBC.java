057b335968a6da2d9448f32a1e648a7b04df8186
jinggajilca <123522983+JinggaJilca@users.noreply.github.com>
2026-06-26 10:02:35 +0700

    Fixing JDBC

--- src/main/java/model/JDBC.java
+++ src/main/java/model/JDBC.java
@@ -1,76 +1,38 @@
-//package model;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//public class JDBC {
-//    
-//    private static final String DATABASE_NAME = "fintrack_db";
-//    private static final String USERNAME = "root";    
-//    private static final String PASSWORD = "root";    
-//    private static final String CONNECTION_URL = 
-//            "jdbc:mysql://localhost:8889/" + DATABASE_NAME + "?useSSL=false&serverTimezone=Asia/Jakarta";
-//    public static Connection getConnection() {
-//        Connection connection = null;
-//        
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//
-//            connection = DriverManager.getConnection(CONNECTION_URL, USERNAME , PASSWORD);
-//        } catch (ClassNotFoundException e) {
-//            System.out.println("Driver tidak ditemukan: " + e.getMessage());
-//        } catch (SQLException e) {
-//            System.out.println("Gagal konek DB: " + e.getMessage());
-//        }
-//        return connection;
-//    }
-//    public static void main(String[] args) {
-//        try (Connection conn = getConnection()) {
-//            
-//            if (conn != null) {
-//                System.out.println("Koneksi ke MySQL berhasil!");
-//            }
-//        } catch (SQLException e) {
-//            System.out.println("Koneksi gagal!");
-//            e.printStackTrace();
-//        }
-//    }
-//}
-
 package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class JDBC {
-    
-    private static final String DATABASE_NAME = "fintrack_db";
-    private static final String USERNAME = "root";    
-    private static final String PASSWORD = "root";    
-    private static final String CONNECTION_URL = 
-            "jdbc:mysql://localhost:8889/" + DATABASE_NAME + "?useSSL=false&serverTimezone=Asia/Jakarta";
-    public static Connection getConnection() {
-        Connection connection = null;
-        
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+   
+   private static final String DATABASE_NAME = "fintrack_db";
+   private static final String USERNAME = "root";    
+   private static final String PASSWORD = "root";    
+   private static final String CONNECTION_URL = 
+           "jdbc:mysql://localhost:8889/" + DATABASE_NAME + "?useSSL=false&serverTimezone=Asia/Jakarta";
+   public static Connection getConnection() {
+       Connection connection = null;
+       
+       try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
 
-            connection = DriverManager.getConnection(CONNECTION_URL, USERNAME , PASSWORD);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver tidak ditemukan: " + e.getMessage());
-        } catch (SQLException e) {
-            System.out.println("Gagal konek DB: " + e.getMessage());
-        }
-        return connection;
-    }
-    public static void main(String[] args) {
-        try (Connection conn = getConnection()) {
-            
-            if (conn != null) {
-                System.out.println("Koneksi ke MySQL berhasil!");
-            }
-        } catch (SQLException e) {
-            System.out.println("Koneksi gagal!");
-            e.printStackTrace();
-        }
-    }
+           connection = DriverManager.getConnection(CONNECTION_URL, USERNAME , PASSWORD);
+       } catch (ClassNotFoundException e) {
+           System.out.println("Driver tidak ditemukan: " + e.getMessage());
+       } catch (SQLException e) {
+           System.out.println("Gagal konek DB: " + e.getMessage());
+       }
+       return connection;
+   }
+   public static void main(String[] args) {
+       try (Connection conn = getConnection()) {
+           
+           if (conn != null) {
+               System.out.println("Koneksi ke MySQL berhasil!");
+           }
+       } catch (SQLException e) {
+           System.out.println("Koneksi gagal!");
+           e.printStackTrace();
+       }
+   }
 }
 
